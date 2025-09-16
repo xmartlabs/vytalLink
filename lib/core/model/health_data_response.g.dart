842c8014981692ed1aa9c8186829391045149195
mirland <mirland@xmartlabs.com>
2025-09-16 11:53:24 -0300

    Some changes

--- lib/core/model/health_data_response.g.dart
+++ lib/core/model/health_data_response.g.dart
@@ -36,36 +36,6 @@ Map<String, dynamic> _$$HealthDataResponseImplToJson(
       'statistic_type': instance.statisticType,
     };
 
-_$AggregatedHealthDataResponseImpl _$$AggregatedHealthDataResponseImplFromJson(
-        Map<String, dynamic> json) =>
-    _$AggregatedHealthDataResponseImpl(
-      success: json['success'] as bool,
-      count: (json['count'] as num).toInt(),
-      healthData: (json['health_data'] as List<dynamic>)
-          .map((e) => AppHealthDataPoint.fromJson(e as Map<String, dynamic>))
-          .toList(),
-      valueType: json['value_type'] as String,
-      startTime: json['start_time'] as String,
-      endTime: json['end_time'] as String,
-      groupBy: json['group_by'] as String?,
-      isAggregated: json['is_aggregated'] as bool?,
-      statisticType: json['statistic_type'] as String?,
-    );
-
-Map<String, dynamic> _$$AggregatedHealthDataResponseImplToJson(
-        _$AggregatedHealthDataResponseImpl instance) =>
-    <String, dynamic>{
-      'success': instance.success,
-      'count': instance.count,
-      'health_data': instance.healthData.map((e) => e.toJson()).toList(),
-      'value_type': instance.valueType,
-      'start_time': instance.startTime,
-      'end_time': instance.endTime,
-      'group_by': instance.groupBy,
-      'is_aggregated': instance.isAggregated,
-      'statistic_type': instance.statisticType,
-    };
-
 _$HealthDataErrorResponseImpl _$$HealthDataErrorResponseImplFromJson(
         Map<String, dynamic> json) =>
     _$HealthDataErrorResponseImpl(
